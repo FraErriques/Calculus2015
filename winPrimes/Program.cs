2d88778a392d429a0bc4f1ee4a3a5ef89b6893ae
FraErriques <francesco.erriques@gmail.com>
2024-03-06 17:22:24 +0100

    test session from ITFORS1011  on 2024.March.06

--- winPrimes/Program.cs
+++ winPrimes/Program.cs
@@ -95,6 +95,7 @@ namespace winPrimes
         /// <summary>
         /// The main entry point for the application.
         /// tests passed on ITFORS1011 on 2024.02.26
+        /// tests passed on ITFORS1011 on 2024.03.06
         /// </summary>
         [STAThread]
         static void Main()
